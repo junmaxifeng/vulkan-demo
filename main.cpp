bca968da56edfc0660123ceb3a0e97e2403fad2b
junmaxifeng <waddm@foxmail.com>
2021-11-19 17:11:14 +0800

    init

--- main.cpp
+++ main.cpp
@@ -2,10 +2,6 @@
 
 int main() {
     VulkanFrame app;
-    app.setWindowSize(800,600);
-    app.setWindowTitle("vulkan test");
-    app.setShaderPath(ShaderType::VERTEX,"../shaders/vert.spv");
-    app.setShaderPath(ShaderType::FRAGMENT,"../shaders/frag.spv");
 
     try {
         app.run();
